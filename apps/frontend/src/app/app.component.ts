fcedf3da12d9ce7fae1196b21236f0d638ff76cc
therbst <herbst.thorsten@gmail.com>
2024-07-24 12:12:08 +0200

    change folder structure

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
+  standalone:  true,
+  imports:     [NxWelcomeComponent, RouterModule],
+  selector:    'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl:    './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend';
